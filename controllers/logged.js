cf8742c1bdbbe23f4444a010ae8f3bf5fb95d572
Denzel M. Echevarria <denzel.echevarria@gmail.com>
2023-05-22 19:25:36 -0400

    updated

--- controllers/logged.js
+++ controllers/logged.js
@@ -1,23 +1,34 @@
+const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
 
-exports.updateRank = async (req, res) => {
-  const { completed, failed } = req.body;
-
-  try {
-    const { userName } = req.user;
-
-    const user = await User.findOne({ userName });
-
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+module.exports = {
+  getFeed: async (req, res) => {
+    try {
+      res.render('training.njk');
+    } catch (err) {
+      console.log(err);
     }
+  },
 
-    
-    user.rank += completed - failed;
-    await user.save(); 
+  updateRank: async (req, res) => {
+    const { completed, failed } = req.body;
 
-    res.status(200).json({ message: 'Rank updated successfully' });
-  } catch (error) {
-    res.status(500).json({ error: 'An error occurred while updating rank' });
-  }
+    try {
+      const { userName } = req.user; // Assuming the username is available in the request object
+
+      const user = await User.findOne({ userName });
+
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+
+      // Update the rank based on completed and failed sessions
+      user.rank += completed - failed;
+      await user.save(); // Save the updated user back to the database
+
+      res.status(200).json({ message: 'Rank updated successfully' });
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred while updating rank' });
+    }
+  },
 };
