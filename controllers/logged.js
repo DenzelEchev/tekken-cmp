87b8bf01936bb4dc792ae41974db32deef913629
Denzel M. Echevarria <denzel.echevarria@gmail.com>
2023-05-23 08:20:04 -0400

    added player schema and added a way to add to collections

--- controllers/logged.js
+++ controllers/logged.js
@@ -1,4 +1,4 @@
-const Post = require("../models/Post");
+const PlayerStats = require("../models/PlayerStats");
 const User = require("../models/User");
 
 module.exports = {
@@ -21,22 +21,41 @@ module.exports = {
 
   updateRank: async (req, res) => {
     const { completed, failed } = req.body;
-
+  
     try {
       const { userName } = req.user;
-
+  
       const user = await User.findOne({ userName });
-
+  
       if (!user) {
         return res.status(404).json({ error: "User not found" });
       }
-
+  
       user.rank += completed - failed;
+      user.sessionCompleted + 1
       await user.save();
-
+  
+      let playerStats = await PlayerStats.findOne({ user: user._id });
+  
+      if (playerStats) {
+        // If playerStats document exists, update the rank and sessionCompleted ONLY
+        playerStats.rank = user.rank;
+        playerStats.sessionCompleted += completed;
+        await playerStats.save();
+      } else {
+        // If playerStats document doesn't exist, create a new one
+        playerStats = new PlayerStats({
+          user: user._id,
+          rank: user.rank,
+          sessionCompleted: completed,
+          character: user.character
+        });
+        await playerStats.save();
+      }
+  
       res.status(200).json({ message: "Rank updated successfully" });
     } catch (error) {
       res.status(500).json({ error: "An error occurred while updating rank" });
     }
-  },
+  }
 };
---
+++ models/PlayerStats.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const PlayerStatsSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  rank: {
+    type: Number,
+    default: 0,
+  },
+  sessionCompleted: {
+    type: Number,
+    default: 0,
+  },
+  character: {
+    type: String,
+    default: "",
+  },
+});
+
+module.exports = mongoose.model("PlayerStats", PlayerStatsSchema);
